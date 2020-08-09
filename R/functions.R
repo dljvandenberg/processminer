308991bbe052b3aec25337762ac6fcadd46cde2f
dljvandenberg <dljvandenberg@gmail.com>
2020-08-09 19:51:44 +0200

    Feature/first app (#6)
    * [DEMO-APP] Initial version of app
    * [REFACTOR] Restructure dirs
    * [REFACTOR] Point to correct source file
    * [DOCKERFILE] Init Dockerfile
    * [DOCKERFILE] Dockerfile for first working app version
    * [DOCKERFILE] Cleanup obsolete comments
    * [LAYOUT] Tweak input fields
    * [PROCESSMAP] Allow choosing between case view and troughput time view
    * [DOCUMENTATION] Add comments to Dockerfile

---
+++ R/functions.R
@@ -0,0 +1,121 @@
+## Define Shiny app
+
+library(processmapR)
+library(processanimateR)
+library(shiny)
+library(shinycssloaders)
+
+process_viewer <- function(eventlog, min.time = 30, max.time = 600, default.time = 60) {
+  
+  ui <- function(request) {
+    fluidPage(
+      tags$head(tags$style("#process{height:90vh !important;}")),
+      titlePanel(title = "ProcessMiner 0.1", 
+                 windowTitle = "ProcessMiner 0.1"),
+      h5(tags$em("A project by Dennis van den Berg")),
+      
+      sidebarLayout(
+        
+        sidebarPanel(
+          width = 2,
+          selectInput("mapType", "Map type", c("cases", "durations"), "cases"),
+          selectInput("timelineMode", "Timeline mode", c("relative", "absolute"), "relative"),
+          selectInput("sizeAttribute", "Size attribute", c("none", colnames(eventlog)), "none"),
+          selectInput("colorAttribute", "Color attribute", c("none", colnames(eventlog)), "none"),
+          selectInput("orientation", "Orientation", c("horizontal"="LR", "vertical"="TB"), "horizontal"),
+          sliderInput("duration", "Animation duration", min.time, max.time, default.time),
+          h4("Selected cases"),
+          textOutput("token_selection"),
+          h4("Selected activities"),
+          textOutput("activity_selection")
+        ),
+        
+        mainPanel(
+          width = 10,
+          shinycssloaders::withSpinner(processanimaterOutput("process"))
+        )
+      )
+    )
+  }
+  
+  server <- function(session, input, output) {
+    
+    data <- reactive({
+      
+      if (input$colorAttribute != "none") {
+        attr <- rlang::sym(input$colorAttribute)
+        val <- eventlog %>% pull(!!attr)
+        if (!(is.character(val) || is.factor(val))) {
+          warning("Trying to use a numeric attribute for the token color!")
+        }
+      }
+      
+      if (input$sizeAttribute != "none") {
+        # This only works for numeric attributes
+        attr <- rlang::sym(input$sizeAttribute)
+        val <- eventlog %>% pull(!!attr)
+        if (!is.numeric(val)) {
+          warning("Trying to use a non-numeric attribute for the token size!")
+        }
+      }
+      
+      eventlog
+      
+    })
+    
+    output$token_selection <- renderText({
+      
+      paste0(input$process_tokens, ",")
+      
+    })
+    
+    output$activity_selection <- renderText({
+      
+      paste0(input$process_activities, ",")
+      
+    })
+    
+    output$process <- renderProcessanimater(expr = {
+      if(input$mapType == "durations"){
+        graph <- processmapR::process_map(data(), render = FALSE, type = performance())
+      } else {
+        graph <- processmapR::process_map(data(), render = FALSE)
+      }
+      model <- DiagrammeR::add_global_graph_attrs(graph, attr = "rankdir", value = input$orientation, attr_type = "graph")
+      if (input$sizeAttribute != "none" && input$colorAttribute != "none") {
+        animate_process(data(), model,
+                        mode = input$timelineMode,
+                        legend = "color",
+                        mapping = token_aes(color = token_scale(input$colorAttribute, scale = "ordinal", 
+                                                                range = RColorBrewer::brewer.pal(5, "YlOrBr")),
+                                            size = token_scale(input$sizeAttribute, scale = "linear", range = c(6,10))),
+                        duration = input$duration,
+                        token_callback_select = token_select_decoration(stroke = "red"))
+      } else if (input$sizeAttribute != "none") {
+        animate_process(data(), model,
+                        mode = input$timelineMode,
+                        legend = "size",
+                        mapping = token_aes(size = token_scale(input$sizeAttribute, scale = "linear", range = c(6,10))),
+                        duration = input$duration,
+                        token_callback_select = token_select_decoration(stroke = "red"))
+      } else if (input$colorAttribute != "none") {
+        animate_process(data(), model,
+                        mode = input$timelineMode,
+                        legend = "color",
+                        mapping = token_aes(color = token_scale(input$colorAttribute, scale = "ordinal", range = RColorBrewer::brewer.pal(5, "YlOrBr"))),
+                        duration = input$duration,
+                        token_callback_select = token_select_decoration(stroke = "red"))
+      } else {
+        animate_process(data(), model,
+                        mode = input$timelineMode,
+                        duration = input$duration,
+                        token_callback_select = token_select_decoration(stroke = "red"))
+      }
+      
+    })
+    
+  }
+  
+  shinyApp(ui, server, options = list(height = 500))
+  
+}
