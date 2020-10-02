270029a02c4b929ddd0d185897865cc5768fb9cb
Dennis van den Berg <d.vandenberg@anchormen.nl>
2020-10-02 22:35:40 +0200

    [README] Update README

--- R/functions.R
+++ R/functions.R
@@ -148,8 +148,8 @@ process_viewer <- function() {
         solidHeader = TRUE,
         width = 12,
         p("ProcessMiner is a simple web-based process mining tool for exploration (and potentially prediction)."),
-        p("It was created by Dennis van den Berg and uses the bupaR process mining library in R."),
-        p("Its current status is: experimental."),
+        p("It was created by Dennis van den Berg and uses the bupaR process mining library in R, with a UI built in R Shiny."),
+        p("Its current status is: experimental"),
         p("Source code: https://github.com/dljvandenberg/processminer")
     )
   )
