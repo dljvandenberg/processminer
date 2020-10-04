15639772fc14ebc8b71da44e176ae77753ff374c
Dennis van den Berg <d.vandenberg@anchormen.nl>
2020-10-04 21:43:01 +0200

    [REFACTOR] Move helper functions to utils.R

--- R/server.R
+++ R/server.R
@@ -18,24 +18,6 @@ library(RColorBrewer)
 
 
 
-### Helper functions
-
-# Add 'time_since_start' column to eventlog
-add_time_since_case_start <- function(eventlog, units = "days") {
-    # Determine timestamp variable from eventlog
-    timestamp_var <- sym(bupaR::timestamp(eventlog))
-    
-    # Add time_since_start per event, since case start
-    eventlog_extended <- eventlog %>% 
-        bupaR::group_by_case() %>% 
-        mutate(time_since_start = difftime(!!timestamp_var, min(!!timestamp_var, na.rm = TRUE), units = units)) %>% 
-        bupaR::ungroup_eventlog()
-    
-    return(eventlog_extended)
-}
-
-
-
 ### Server logic
 
 server <- function(session, input, output) {
--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+source('R/utils.R')
 source('R/mod_eventlog_summary.R')
 source('R/mod_processflow.R')
 source('R/mod_eventlog_timeline.R')
