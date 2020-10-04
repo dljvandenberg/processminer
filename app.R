6ce01b0d68a74f8a99c058f3a705ec1a2ba41b72
Dennis van den Berg <d.vandenberg@anchormen.nl>
2020-10-04 21:53:07 +0200

    [REFACTOR] Simplify dir structure

--- app.R
+++ app.R
@@ -1,12 +1,2 @@
-source('R/utils.R')
-source('R/mod_eventlog_summary.R')
-source('R/mod_processflow.R')
-source('R/mod_eventlog_timeline.R')
-source('R/ui.R')
-source('R/server.R')
-
-
-
-### Start app
-
-shinyApp(ui, server, options = list(height = 500))
+# Start app (sources ui.R, server.R and R/*.R)
+runApp()
