d4b1c14bff61aca803e5549feccc4784a29b87f0
Dennis van den Berg <d.vandenberg@anchormen.nl>
2020-10-04 22:05:04 +0200

    Revert "[REFACTOR] Simplify dir structure"
    This reverts commit 6ce01b0d68a74f8a99c058f3a705ec1a2ba41b72.

--- app.R
+++ app.R
@@ -1,2 +1,12 @@
-# Start app (sources ui.R, server.R and R/*.R)
-runApp()
+source('R/utils.R')
+source('R/mod_eventlog_summary.R')
+source('R/mod_processflow.R')
+source('R/mod_eventlog_timeline.R')
+source('R/ui.R')
+source('R/server.R')
+
+
+
+### Start app
+
+shinyApp(ui, server, options = list(height = 500))
