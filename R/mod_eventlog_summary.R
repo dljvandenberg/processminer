b92005fc126014815ef5263eb2545f7aca2ced5b
dljvandenberg <dljvandenberg@gmail.com>
2020-10-11 17:14:24 +0200

    Feature/refactor (#54)
    * [REFACTOR] Split app into ui.R, server.R, global.R and functions.R
    * [REFACTOR] Cleanup obsolete data dir
    * [REFACTOR] Rename dir structure
    * [CLEANUP] Remove obsolete notebooks and design draft
    * [REFACTOR] Move summary statistics code to separate module
    * [REFACTOR] Cleanup obsolete libs and comments
    * [REFACTOR] Remove commented out code
    * [REFACTORING] Remove obsolete comments
    * [REFACTOR] Move process flow code to separate module
    * [REFACTOR] Update comments
    * [FIX] Fix height of process flow
    * [LAYOUT] Make link to git repo clickable
    * [REFACTOR] Move events timeline code to separate module
    * [REFACTOR] Rename mod_eventlogsummary.R
    * [REFACTOR] Refactor UI code
    * [REFACTOR] Move helper functions to utils.R
    * [REFACTOR] Add comments, rename
    * [REFACTOR] Rename, fix
    * [REFACTOR] Move table view tab to separate module
    * [REFACTOR] Remove obsolete libraries, rename
    * [FIX] Remove duplicate module
    * [PROCESS-FLOW] Filter on min nr of traces, closes #18
    * [PROCESS-FLOW] Filter on time range, closes #25
    * [LICENSE] Add MIT License
    * [LOAD-DATA] Move data load code to separate module
    * [LOAD-DATA] Fix for passing reactivevalues to load_eventlog module
    * [REFACTOR] Rename myeventlog to eventlog for consistency

---
+++ R/mod_eventlog_summary.R
@@ -0,0 +1,117 @@
+# Shiny Module for eventlog summary
+
+library(dplyr)
+library(tidyr)
+library(bupaR)
+library(edeaR)
+library(shiny)
+library(shinydashboard)
+library(shinycssloaders)
+library(plotly)
+
+
+
+##################################
+## Eventlog summary tab UI code ##
+##################################
+eventlogSummaryTabUI <- function(id){
+  ns <- NS(id)
+
+  body_summary_statistics <- fluidRow(
+    
+    box(title = "Cases per activity",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 6,
+        shinycssloaders::withSpinner(plotly::plotlyOutput(ns("stats_cases")))
+    ),
+    box(title = "Events per activity",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 6,
+        shinycssloaders::withSpinner(plotly::plotlyOutput(ns("stats_events")))
+    ),
+    box(title = "Throughput times per case",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 6,
+        shinycssloaders::withSpinner(plotOutput(ns("throughput_time_plot")))
+    ),
+    box(title = "Summary stats",
+        status = "primary",
+        solidHeader = TRUE,
+        width = 6,
+        shinycssloaders::withSpinner(tableOutput(ns("stats_table")))
+    )
+  )
+  
+  tabItem(tabName = "summary_statistics", body_summary_statistics)
+}
+
+
+
+######################################
+## Eventlog summary tab server code ##
+######################################
+eventlogSummaryTab <- function(input, output, session, eventlog, default_color = "skyblue2"){
+  ns <- session$ns
+  
+  
+  output$stats_cases <- plotly::renderPlotly({
+    
+    req(eventlog())
+    
+    activity_var <- sym(bupaR::activity_id(eventlog()))
+    eventlog() %>% 
+      bupaR::group_by_activity() %>% 
+      bupaR::n_cases() %>% 
+      {ggplot(., aes(x = !!activity_var, y = n_cases)) +
+          geom_col(fill = default_color) +
+          ylab('Number of cases') +
+          coord_flip()} %>% 
+      ggplotly()
+  })
+  
+  
+  output$stats_events <- plotly::renderPlotly({
+    
+    req(eventlog())
+    
+    activity_var <- sym(bupaR::activity_id(eventlog()))
+    eventlog() %>% 
+      bupaR::group_by_activity() %>% 
+      bupaR::n_events() %>% 
+      {ggplot(., aes(x = !!activity_var, y = n_events)) +
+          geom_col(fill = default_color) +
+          ylab('Number of events') +
+          coord_flip()} %>% 
+      ggplotly()
+  })
+  
+  
+  output$throughput_time_plot <- renderPlot({
+    
+    req(eventlog())
+    
+    eventlog() %>% 
+      edeaR::throughput_time(level = "case") %>% 
+      plot()
+    
+  })
+  
+  
+  output$stats_table <- shiny::renderTable({
+    
+    req(eventlog())
+    
+    n_cases <- bupaR::n_cases(eventlog())
+    n_events <- bupaR::n_events(eventlog())
+    n_activities <- bupaR::n_activities(eventlog())
+    
+    data.frame(Statistic = c('Number of activities', 'Number of cases', 'Number of events'), Value = c(n_activities, n_cases, n_events))
+  })
+  
+  
+  menuItem(text = "Summary statistics", tabName = "summary_statistics", icon = icon("chart-bar"))
+  
+}
